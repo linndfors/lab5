c6c8eab87741fbfa560223c9cdc64c5322b3531b
Olli <91615532+linndfors@users.noreply.github.com>
2022-10-21 18:08:39 +0300

    Update StoreTest.java

--- src/test/java/flower/store/StoreTest.java
+++ src/test/java/flower/store/StoreTest.java
@@ -4,38 +4,38 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 class StoreTest {
-    private static final int price = 13;
-    private static final int pricei = 11;
-    private static final int priceb = 10;
+    private static final int PRICE = 13;
+    private static final int PRICEI = 11;
+    private static final int PRICEB = 10;
     @Test
     public void testStore() {
         Store store = new Store();
         Flower fl = new Flower();
-        fl.setPrice(price);
+        fl.setPrice(PRICE);
         fl.setColor(FlowerColor.RED);
         store.add(fl);
         Flower fll = new Flower();
-        fll.setPrice(price);
+        fll.setPrice(PRICE);
         fll.setColor(FlowerColor.BLUE);
         store.add(fll);
         Flower flll = new Flower();
-        flll.setPrice(price);
+        flll.setPrice(PRICE);
         flll.setColor(FlowerColor.RED);
         store.add(flll);
         Flower flwr = new Flower();;
-        flwr.setPrice(pricei);
+        flwr.setPrice(PRICEI);
         flwr.setColor(FlowerColor.RED);
         store.add(flwr);
         Flower flw = new Flower();
-        flw.setPrice(price);
+        flw.setPrice(PRICE);
         flw.setColor(FlowerColor.RED);
         Assertions.assertEquals(store.search(flw), 2);
         Flower flo = new Flower();
-        flo.setPrice(priceb);
+        flo.setPrice(PRICEB);
         flo.setColor(FlowerColor.BLUE);
         Assertions.assertEquals(store.search(flo), 0);
         Flower flb = new Flower();
-        flb.setPrice(price);
+        flb.setPrice(PRICE);
         flb.setColor(FlowerColor.BLUE);
         Assertions.assertEquals(store.search(flb), 1);
     }
